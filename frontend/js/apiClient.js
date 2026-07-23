953f788d51193f4de50a83107c44e68a272846eb
Francisco <56210099+francisco-olvdev@users.noreply.github.com>
2026-07-22 22:52:20 -0300

    Corrigindo API

--- frontend/js/apiClient.js
+++ frontend/js/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "bora-formar-api-ergrardbhvhwcpa6.eastus-01.azurewebsites.net";
 
 function getToken() {
     return localStorage.getItem("token");
